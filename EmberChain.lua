313eb35c6bdf641f727a6bcdebbe1e5b7d9c3b96
hake-me <admin@mg.hake.me>
2017-02-01 04:12:40 -0800

    Heavily simplified code, and made it more reliable (it was spamming before)

--- EmberChain.lua
+++ EmberChain.lua
@@ -3,45 +3,41 @@ local ember= {}
 ember.optionEnable = Menu.AddOption({ "Hero Specific","Ember"}, "Enabled", "Auto Chain")
 ember.optionKey = Menu.AddKeyOption({ "Hero Specific","Ember"}, "Key", Enum.ButtonCode.KEY_P)
 
+-- this will be set later (only once) inside of OnUpdate.
+ember.chainsRadius = 0
+
 function ember.OnUpdate()
 	if not Menu.IsEnabled(ember.optionEnable) then return end
 	if not Menu.IsKeyDown(ember.optionKey) then return end
+    
 	local myHero = Heroes.GetLocal()
-	if NPC.GetUnitName(myHero)~="npc_dota_hero_ember_spirit" then return end
+
+	if NPC.GetUnitName(myHero) ~= "npc_dota_hero_ember_spirit" then return end
 
 	local punch = NPC.GetAbilityByIndex(myHero, 1)
 	local fist = NPC.GetAbilityByIndex(myHero, 0)
+
 	local mousePos = Input.GetWorldCursorPos()
-	Ability.CastPosition(punch, mousePos)
-	local enemy = getCloseEnemy(mousePos,myHero) 
-	local enemyPos = NPC.GetAbsOrigin(enemy)
-	local myPos = NPC.GetAbsOrigin(myHero)
-	local distance = myPos:Distance(enemyPos);
-          distance =  distance:Length2DSqr()
-    Log.Write(distance)
-    if (distance<6000) then
+    local myMana = NPC.GetMana(myHero)
+
+    if punch ~= nil and Ability.IsCastable(punch, myMana) then
+	    Ability.CastPosition(punch, mousePos)
+    end
+
+    if fist == nil or not Ability.IsCastable(fist, myMana) then return end
+
+    -- since ember spirit's chains ability doesn't increase in radius each level, just get it once here.
+    -- there would be no point in doing it every tick!
+    if ember.chainsRadius == 0 then
+        ember.chainsRadius = Ability.GetLevelSpecialValueFor(fist, "radius")
+    end
+
+    -- get the nearest enemy to cursor.
+	local enemy = Input.GetNearestUnitToCursor(Entity.GetTeamNum(myHero), Enum.TeamType.TEAM_ENEMY)
+
+    if enemy ~= nil and NPC.IsEntityInRange(myHero, enemy, ember.chainsRadius) then
     	Ability.CastNoTarget(fist)
     end
-end
-
-function getCloseEnemy(mousePos, myHero)
-	local target;
-	local minDistance = 1000000;
-	for i = 1, Heroes.Count() do
-		local hero = Heroes.Get(i)
-		local myTeam = Entity.GetTeamNum(myHero)
-        local sameTeam = Entity.GetTeamNum(hero) == myTeam
-          if not sameTeam and not NPC.IsDormant(hero) and Entity.GetHealth(hero) > 0 then
-            local enemyPos = NPC.GetAbsOrigin(hero)
-            local distance = mousePos:Distance(enemyPos);
-            distance =  distance:Length2DSqr()
-            if distance < minDistance then
-            	target = hero;
-            	minDistance = distance
-            end
-        end
-	end
-	return target
 end
 
 return ember

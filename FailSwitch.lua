12fb6cf8e308281b6d642840e875e1b05619ed98
Jiwei Xia <jiweixia2016@u.northwestern.edu>
2017-02-03 18:36:23 -0800

    Void added, no Hero ristriction

--- FailSwitch.lua
+++ FailSwitch.lua
@@ -1,26 +1,26 @@
 local failSwitch= {}
 
-failSwitch.optionEnable = Menu.AddOption({ "Utility","failSwitch"}, "Enable", "Stop ultimate if no enemy in radius")
-failSwitch.optionKey = Menu.AddKeyOption({ "Utility","failSwitch"}, "Force Cast Key",Enum.ButtonCode.KEY_P)
+failSwitch.optionEnable = Menu.AddOption({ "Utility","Fail Switch"}, "Enable", "Stop ultimate if no enemy in radius")
+failSwitch.optionKey = Menu.AddKeyOption({ "Utility","Fail Switch"}, "Force Cast Key",Enum.ButtonCode.KEY_P)
 
-failSwitch.ultiRadius = {enigma_black_hole = 420, magnataur_reverse_polarity = 410}
+failSwitch.ultiRadius = {enigma_black_hole = 420, magnataur_reverse_polarity = 410, faceless_void_chronosphere = 425}
 
 function failSwitch.OnUpdate()
 	if not Menu.IsEnabled(failSwitch.optionEnable) then return true end
 	if not Menu.IsKeyDown(failSwitch.optionKey) then return end
 
 	local myHero = Heroes.GetLocal()
-	local name = NPC.GetUnitName(myHero)
+	
 	local mousePos = Input.GetWorldCursorPos()
 
-	if not (name == "npc_dota_hero_enigma" or name == "npc_dota_hero_magnataur") then return end
 	local myMana = NPC.GetMana(myHero)
 	local ulti = NPC.GetAbilityByIndex(myHero, 3)
-	Log.Write(Ability.GetName(ulti))
 	if ulti ~= nil and Ability.IsCastable(ulti, myMana) then
-		if name == "npc_dota_hero_enigma" then
+		local name =Ability.GetName(ulti)
+		Log.Write(name)
+		if name == "enigma_black_hole" or name == "faceless_void_chronosphere" then
         	Ability.CastPosition(ulti, mousePos)
-        else 
+        elseif name == "magnataur_reverse_polarity" then
         	Ability.CastNoTarget(ulti)
         end
     end
@@ -32,7 +32,7 @@ function failSwitch.OnPrepareUnitOrders(orders)
     if not orders.ability then return true end
     if not (orders.order == Enum.UnitOrder.DOTA_UNIT_ORDER_CAST_POSITION or orders.order == Enum.UnitOrder.DOTA_UNIT_ORDER_CAST_NO_TARGET) then return true end
     local abilityName = Ability.GetName(orders.ability)
-    if not ( abilityName == "enigma_black_hole" or abilityName == "magnataur_reverse_polarity") then return true end
+    if not ( abilityName == "enigma_black_hole" or abilityName == "magnataur_reverse_polarity" or abilityName == "faceless_void_chronosphere") then return true end
 	local myHero = Heroes.GetLocal()
 	local mousePos = Input.GetWorldCursorPos()
     local enemy = Input.GetNearestHeroToCursor(Entity.GetTeamNum(myHero), Enum.TeamType.TEAM_ENEMY)

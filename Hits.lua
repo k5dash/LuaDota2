40052012255fa595d125181d86200448b2769809
Jiwei Xia <jiweixia2016@u.northwestern.edu>
2017-02-03 18:52:55 -0800

    FontSize Change

--- Hits.lua
+++ Hits.lua
@@ -1,17 +1,23 @@
 local hit= {}
 
-hit.optionEnable = Menu.AddOption({ "Awareness"}, "Show Hit Count", "Show how many hit at most to kill the enemy")
-hit.font = Renderer.LoadFont("Tahoma", 25, Enum.FontWeight.EXTRABOLD)
-
+hit.optionEnable = Menu.AddOption({ "Awareness", "Show Hit Count"}, "Enable", "Show how many hit at most to kill the enemy")
+hit.fontSize = Menu.AddOption({ "Awareness", "Show Hit Count"}, "Font Size", "", 20, 50, 10)
+hit.font = Renderer.LoadFont("Tahoma", 30, Enum.FontWeight.EXTRABOLD)
+hit.fontSave = 30;
 function hit.OnDraw()
     if not Menu.IsEnabled(hit.optionEnable) then return end
     local myHero = Heroes.GetLocal()
     local enemy = Input.GetNearestHeroToCursor(Entity.GetTeamNum(myHero), Enum.TeamType.TEAM_ENEMY)
-    getHitNumbers(myHero,enemy)
-    getHitNumbers(enemy,myHero)
+    hit.getHitNumbers(myHero,enemy)
+    hit.getHitNumbers(enemy,myHero)
+    local fontNow =  Menu.GetValue(hit.fontSize)
+    if (fontNow ~= fontSave) then
+        hit.font = Renderer.LoadFont("Tahoma", fontNow, Enum.FontWeight.EXTRABOLD)
+        fontSave = fontNow
+    end 
 end
 
-function getHitNumbers(myHero, enemy)
+function hit.getHitNumbers(myHero, enemy)
     if myHero == nil or enemy==nil then return end
     local trueDamage = NPC.GetDamageMultiplierVersus(myHero, enemy) * (NPC.GetTrueDamage(myHero) * NPC.GetArmorDamageMultiplier(enemy))
     local pos = NPC.GetAbsOrigin(enemy)
@@ -23,4 +29,6 @@ function getHitNumbers(myHero, enemy)
         Renderer.DrawTextCentered(hit.font, x, y, hitCount, 1)
     end
 end
+
+
 return hit
